9146ee534a92726910f216ca6da3544f25caa828
Kris Parag <kvpmea@gmail.com>
2024-05-15 09:34:27 +0100

    Add files via upload

--- main/defaultSet.m
+++ main/defaultSet.m
@@ -4,6 +4,6 @@ function [grey1, grey2, cmap, fnt] = defaultSet(nCol)
 % Set figure defaults
 set(groot, 'defaultAxesTickLabelInterpreter', 'latex', 'defaultLegendInterpreter', 'latex');
 % Font sizes
-fnt = 25; set(0, 'defaultTextInterpreter', 'latex', 'defaultAxesFontSize', fnt);
+fnt = 22; set(0, 'defaultTextInterpreter', 'latex', 'defaultAxesFontSize', fnt);
 % New colour choices
 cmap = linspecer(nCol, 'sequential'); grey1 = 0.8*ones(1, 3); grey2 = 0.5*ones(1, 3);
